21dfdbcc8f82660f960a18a762ca117174256cd0
HuynhAnhKhoi2052001 <anhkhoi2052001@gmail.com>
2024-03-29 10:06:04 +0700

    add redis

--- login_service/src/main/java/com/example/login_service/controllers/LoginController.java
+++ login_service/src/main/java/com/example/login_service/controllers/LoginController.java
@@ -2,19 +2,16 @@ package com.example.login_service.controllers;
 
 import com.example.login_service.models.User;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
 
 @RestController
+@RequestMapping("/login")
 public class LoginController {
-    @GetMapping("/login/check-user")
+    @GetMapping("/check-user")
     public User checkUser(@RequestParam("username") String username, @RequestParam("password") String password) {
         RestTemplate restTemplate = new RestTemplate();
         String apiUrl = "http://localhost:8081/register/checkUser?username=" + username + "&password=" + password;
@@ -28,20 +25,23 @@ public class LoginController {
         return responseEntity.getBody();
     }
 
-    @PostMapping("/login/login")
-    public String login(@RequestParam("username") String username, @RequestParam("password") String password) {
-        RestTemplate restTemplate = new RestTemplate();
-        String apiUrl = "http://localhost:8081/register/checkUser?username=" + username + "&password=" + password;
-        ResponseEntity<User> responseEntity = restTemplate.exchange(
-                apiUrl,
-                HttpMethod.GET,
-                null,
-                new ParameterizedTypeReference<User>() {
-                }
-        );
-        User user = responseEntity.getBody();
+    @PostMapping("/login")
+    public String login(@RequestParam("username") String username, @RequestParam("password") String password)  {
+        User user = checkUser(username,password);
         if(user !=null)
             return "Đăng nhập thành công";
         else return "Đăng nhập thất bại";
+    }
+    @PostMapping("/save")
+    public String saveUser(@RequestBody User user){
+        RestTemplate restTemplate = new RestTemplate();
+        String apiUrl ="http://localhost:8081/register/save";
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        HttpEntity<User> httpEntity = new HttpEntity<>(user,headers);
+        ResponseEntity<User> responseEntity = restTemplate.exchange(apiUrl, HttpMethod.POST, httpEntity, new ParameterizedTypeReference<User>() {});
+        if(responseEntity.getBody()== null)
+            return "Lưu thất bại";
+        else return "Lưu thành công";
     }
 }
--- register-service/src/main/java/com/example/registerservice/controllers/UserController.java
+++ register-service/src/main/java/com/example/registerservice/controllers/UserController.java
@@ -6,22 +6,38 @@ import org.apache.tomcat.util.buf.UEncoder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import redis.clients.jedis.Jedis;
 
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/register")
 public class UserController {
     @Autowired
     UserRepository userRepository;
+    Jedis jedis = new Jedis();
 
-    @PostMapping("/register/save")
+    @PostMapping("/save")
     public User saveUser(@RequestBody User user) {
+        String key = String.valueOf(user.getUserId());
+        jedis.hset(key,"username",user.getUserName());
+        jedis.hset(key,"password",user.getPassword());
+        jedis.hset(key,"email",user.getEmail());
         userRepository.save(user);
         return user;
     }
 
-    @GetMapping("/register/getId")
+    @GetMapping("/getId")
     public ResponseEntity<User> getUserById(@RequestParam("id") Long id) {
+        String key = String.valueOf(id);
+        if(jedis.exists(key)){
+            User userInCache = new User();
+            userInCache.setUserId(id);
+            userInCache.setUserName(jedis.hget(key,"username"));
+            userInCache.setPassword(jedis.hget(key,"password"));
+            userInCache.setEmail(jedis.hget(key,"email"));
+            return  ResponseEntity.ok(userInCache);
+        }
         Optional<User> userOptional = userRepository.findById(id);
         if (userOptional.isEmpty())
             return ResponseEntity.notFound().build();
@@ -29,7 +45,7 @@ public class UserController {
             return ResponseEntity.ok(userOptional.get());
     }
 
-    @GetMapping("/register/checkUser")
+    @GetMapping("/checkUser")
     public User checkUser(@RequestParam("username") String username, @RequestParam("password") String password) {
         User user = userRepository.findByUserNameAndPassword(username, password);
         return user;
